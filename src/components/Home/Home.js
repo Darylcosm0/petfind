76686b8232cc2b04b62e98fe1798eb57b834f2e5
Darylcosm0 <mutaidaryl@gmail.com>
2023-01-30 10:19:02 +0300

    fixed home page and added comments to code

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,40 +2,49 @@ import React from "react";
 import dogi from "../../images/dogi.jpg";
 import "./Home.css";
 
+// Home component that returns a div with container and text class
 function Home() {
   return (
     <div className="section-a">
       <div className="container">
+        {/* Image component */}
         <div className="image-kom">
           <img src={dogi} alt="img" />
         </div>
 
         <div className="text">
-          <h4>Organic TEA</h4>
+          {/* Emojis */}
+          👩🏼‍🦰 🧑🏼‍🦰
+          {/* Header component */}
           <h1 className="mega-font">
             Hello
             <span> Pet Lovers</span>
           </h1>
+          {/* Paragraph component */}
           <p style={{ fontSize: "30px" }}>
             An online platform to search for animals who need homes. A directory
             of animal shelters and adoption organizations in Canada.
           </p>
 
-          <p className="number">$ 89.99</p>
+          {/* Empty paragraph component */}
+          <p className="number"></p>
+          {/* Horizontal line component */}
           <hr width="50%" />
-          <a href="#" className="button">
-            Explore Now
-          </a>
+          
+          {/* Column grid component */}
           <div className="column-grid">
             <div className="column-1">
+              {/* Column component */}
               <h3>Dogs</h3>
               <p>Domestic Animals</p>
             </div>
             <div className="column-2">
+              {/* Column component */}
               <h3>Cats</h3>
               <p>Tamed Animals</p>
             </div>
             <div className="column-3">
+              {/* Column component */}
               <h3>Family Friendly</h3>
               <p>kids safe</p>
             </div>
